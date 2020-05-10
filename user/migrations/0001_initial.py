a61d3b83ee632304457160c0fa8e63d4c8932018
Marinella <marinella16@ru.is>
2020-05-10 17:34:48 +0000

    login og register fix

--- user/migrations/0001_initial.py
+++ user/migrations/0001_initial.py
@@ -20,6 +20,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('profile_image', models.CharField(max_length=9999)),
+                ('profile_img', models.ImageField(blank=True)),
                 ('favorite_product', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='products.Product')),
                 ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
