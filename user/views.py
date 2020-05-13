a5f22e34622f940bbd5d8a8d7aba80f458145aa2
Marinella <marinella16@ru.is>
2020-05-13 19:12:41 +0000

    user profile

--- user/views.py
+++ user/views.py
@@ -2,17 +2,29 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from user.forms.profile_form import ProfileForm
 from user.models import Profile
+from django.contrib.auth import (authenticate, login)
 
 
 def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')
-    return render(request, 'user/register.html', {
-        'form': UserCreationForm(),
-    })
+    if request.user.is_authenticated:
+        return redirect('/')
+    next = request.GET.get('next')
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        user = form.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        new_user = authenticate(username=username, password=password)
+        login(request, user)
+        if next:
+            return redirect(next)
+        return redirect('/')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'user/register.html', context)
 
 def edit_profile(request):
     profile = Profile.objects.filter(user=request.user).first()
@@ -28,4 +40,6 @@ def edit_profile(request):
     })
 
 def profile(request):
+    if not request.user.is_authenticated:
+        return redirect('/')
     return render(request, '../templates/user/profile.html')
