d44af678b09525eb9c6b00ab8538b0bdef3c71f1
danielgeir <danielgeir@hotmail.com>
2020-05-15 11:21:16 +0000

    Bla

--- static/js/products.js
+++ static/js/products.js
@@ -48,7 +48,7 @@ $('#carousel-example').on('slide.bs.carousel', function (e) {
         var it = itemsPerSlide - (totalItems - idx);
         for (var i=0; i<it; i++) {
             // append slides to end
-            if (e.direction==="left") {
+            if (e.direction=="left") {
                 $('.carousel-item').eq(i).appendTo('.carousel-inner');
             }
             else {
