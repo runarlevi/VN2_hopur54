346cffbedcce4cb29dc2d6a23689b40db81cc877
Marinella <marinella16@ru.is>
2020-05-15 17:15:31 +0000

    contact form

--- store_info/views.py
+++ store_info/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import ContactForm
 
 # Create your views here.
 def index(request):
@@ -8,4 +10,11 @@ def help(request):
     return render(request, 'store_info/help.html')
 
 def contact_us(request):
-    return render(request, 'store_info/contact_us.html')
+    if request.method == 'POST':
+        form =ContactForm(request.POST)
+        if form.is_valid()():
+            return HttpResponse('Thanks for contacting us!')
+    else:
+        form = ContactForm()
+
+    return render(request, 'store_info/contact_us.html', {'form':form})
