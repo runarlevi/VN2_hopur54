3a563288b7bf44cf877ab68d1c8ae5f68e0b03d8
Marinella <marinella16@ru.is>
2020-05-15 17:06:55 +0000

    help urlar

--- store_info/urls.py
+++ store_info/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     # http://localhost:8000/storeinfo
     path('', views.index, name="store-info-index"),
+    path('help/', views.help, name="help"),
+    path('contact_us/', views.contact_us, name="contact_us"),
+
 ]
--- store_info/views.py
+++ store_info/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'store_info/index.html')
+
+def help(request):
+    return render(request, 'store_info/help.html')
+
+def contact_us(request):
+    return render(request, 'store_info/contact_us.html')
